ca105f4bd417d65765c46fa62b557a094ca39c6f
John Hadikusumo <john.hadikusumo@gmail.com>
2022-05-22 19:47:49 +1000

    saved it

---
+++ KiwiSoft.Bitly.Sample1/Program.cs
@@ -0,0 +1,12 @@
+﻿using KiwiSoft.Bitly;
+using KiwiSoft.Bitly.Models;
+using KiwiSoft.Bitly.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+ServiceCollection serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped<IShortener, Shortener>();
+
+IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+IShortener shortener = serviceProvider.GetRequiredService<IShortener>();
+ReturnURL result = shortener.Create("https://cmatskas.com/create-an-azure-ad-protected-api-that-calls-into-cosmosdb-with-azure-functions-and-net-core-3-1/");
+Console.WriteLine(result.Url);
